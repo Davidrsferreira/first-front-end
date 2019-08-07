5bf7c86f0a6ced83dc62bb73511a5bbe6676a28b
David Ferreira <davidrsferreira@gmail.com>
2019-08-07 15:55:38 +0100

    List objects in table

--- javabank/js/main.js
+++ javabank/js/main.js
@@ -1,50 +1,41 @@
-window.onload = function () {
+$(document).ready(function() {
 
-    var ajax;
-
-    if (window.XMLHttpRequest) {
-        // Mozilla, Safari, IE7+ ...
-        ajax = new XMLHttpRequest();
-    } else if (window.ActiveXObject) {
-        // IE 6 and older
-        ajax = new ActiveXObject('Microsoft.XMLHTTP');
+    function successCallback(response) {
+        populateTable(response);
     }
-    // run this when the ajax request completes
-    ajax.onreadystatechange = function () {
-        if (ajax.readyState === 4 && ajax.status === 200) {
-            alert(ajax.responseText);
+    
+    function errorCallback(request, status, error) {
+        // do something with the error
+    }
+    
+    // perform an ajax http get request
+    $.ajax({
+        url: 'http://localhost:8080/javabank/api/customer/',
+        async: true,
+        success: successCallback,
+        error: errorCallback
+    });
+
+});
+
+function populateTable(response) {
+
+    var element = '';
+
+    for (var i = 0; i < response.length; i++) {
+        
+        element += '<tr>';
+
+        for (var j = 0; j < response.length; j++){
+            element += '<td>' + Object.values(response) + '</td>';
         }
-    };
 
-    // start the AJAX request
-    ajax.open('GET', 'http:/localhost:8080/javabank/api/customer/', true);
-    ajax.setRequestHeader('Content-type', 'application/json');
-    ajax.send();
-}
-
-/*
-    var customerData = [
-        { "id": 1, "firstName": "Rui", "lastName": "Ferrão", "email": "rui@gmail.com", "phone": "777888" },
-        { "id": 2, "firstName": "Sergio", "lastName": "Gouveia", "email": "sergio@gmail.com", "phone": "777999" },
-        { "id": 3, "firstName": "Bruno", "lastName": "Ferreira", "email": "bruno@gmail.com", "phone": "777666" },
-        { "id": 4, "firstName": "Rodolfo", "lastName": "Matos", "email": "rodolfo@gmail.com", "phone": "777333" }
-    ];
-
-    var usersTable = document.getElementById('users-table');
-
-    for (var i = 0; i < customerData.length; i++) {
-        var row = usersTable.insertRow(-1);
-        row.insertCell(0).innerHTML = customerData[i].firstName;
-        row.insertCell(1).innerHTML = customerData[i].lastName;
-        row.insertCell(2).innerHTML = customerData[i].email;
-        row.insertCell(3).innerHTML = customerData[i].phone;
-        row.insertCell(4).innerHTML = '<button type="button" onclick="helloWorldFn()" class="btn btn-primary">Edit</button>';
-        row.insertCell(5).innerHTML = '<button type="button" class="btn btn-danger">Delete</button>';
+        element += '<td><button type="button" onclick="helloWorldFn()" class="btn btn-primary">Edit</button></td>';
+        element += '<td><button type="button" class="btn btn-danger">Delete</button></td>';
+        element += '</tr>';
+    
     }
 
+    $(element).appendTo('#users-table');
+
 }
-
-var helloWorldFn = function () {
-    alert("hello world!");
-};*/
-
